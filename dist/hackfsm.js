be7eb0618fa366331580cbb936f510049dbaeb22
David Su <dtscraft@gmail.com>
2014-04-11 15:06:54 -0700

    basic gallery loading with angular
    To view:
    npm install
    npm start
    then visit localhost:8000/dist/
    You have have to install other missing node dependencies

--- dist/hackfsm.js
+++ dist/hackfsm.js
@@ -1,4 +1,20 @@
-var container = document.querySelector('#container');
-var msnry = new Masonry( container, {
-        itemSelector: '.item'
-});
+'use strict';
+//var container = document.querySelector('#container');
+//var msnry = new Masonry( container, {
+        //itemSelector: '.item'
+//});
+
+/* Controllers */
+
+var galleryApp = angular.module('galleryApp', []);
+
+galleryApp.controller('GalleryGridCtrl', ['$scope', '$http', function($scope, $http) {
+
+  $scope.items = [];
+  $http.get('data/images.json').success(function(data) {
+    //console.log(data);
+    $scope.items = data;
+  });
+
+  //$scope.orderProp = 'age';
+}]);
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,18 @@
-var container = document.querySelector('#container');
-var msnry = new Masonry( container, {
-        itemSelector: '.item'
-});
+'use strict';
+// TODO: integrate masonry with gallery
+//var container = document.querySelector('#container');
+//var msnry = new Masonry( container, {
+        //itemSelector: '.item'
+//});
+
+/* Controllers */
+
+var galleryApp = angular.module('galleryApp', []);
+
+galleryApp.controller('GalleryGridCtrl', ['$scope', '$http', function($scope, $http) {
+
+  $scope.items = [];
+  $http.get('data/images.json').success(function(data) {
+    $scope.items = data;
+  });
+}]);
